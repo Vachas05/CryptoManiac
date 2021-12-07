b79f0fa3cc2167b8de3c2dfc91305b9059cda3f8
Allam Vachaspati <allamvachaspati05@gmail.com>
2021-12-07 20:44:10 +0530

    name change

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ const App = () => {
                 </Layout>
                 <div className="footer">
                     <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
-                        Crypoverse <br />
+                        CryptoManiac <br />
                         All Rights Reserved
                     </Typography.Title>
                     <Space>
