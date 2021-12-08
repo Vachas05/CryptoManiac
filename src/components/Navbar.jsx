8866364f15a9e24ad346cd4e71dae63303dcacd0
Allam Vachaspati <allamvachaspati05@gmail.com>
2021-12-08 20:54:11 +0530

    cryptos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     <div className="nav-container">
       <div className="logo-container">
         <Avatar src={icon} size="large" />
-        <Typography.Title level={2} className="logo">
+        <Typography.Title level={3} className="logo">
           <Link to="/">CryptoManiac</Link>
         </Typography.Title>
       </div>
