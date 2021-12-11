bd72e8b1227671e965032da35f1cb75c853af2be
Allam Vachaspati <allamvachaspati05@gmail.com>
2021-12-11 12:10:12 +0530

    complete

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
+import millify from "millify";
+import { Collapse, Row, Col, Typography, Avatar } from "antd";
+import HTMLReactParser from "html-react-parser";
+
+import { useGetExchangesQuery } from "../services/cryptoApi";
+import Loader from "./Loader";
+
+const { Text,Title } = Typography;
+const { Panel } = Collapse;
 
 const Exchanges = () => {
-    return (
-        <div>
-            Exchan
-        </div>
-    )
-}
+  const { data, isFetching } = useGetExchangesQuery();
+  const exchangesList = data?.data?.exchanges;
 
-export default Exchanges
+  if (isFetching) return <Loader />;
+
+  return (
+    <>
+      <Row>
+        <Col span={6} style={{paddingLeft:'35px'}}><Title level={4}>Exchanges</Title></Col>
+        <Col span={6} style={{textAlign:'center'}}><Title level={4}>24h Trade Volume</Title></Col>
+        <Col span={6} style={{textAlign:'center'}}><Title level={4}>Markets</Title></Col>
+        <Col span={6} style={{textAlign:'center'}}><Title level={4}>Change</Title></Col>
+      </Row>
+      <Row>
+        {exchangesList.map((exchange) => (
+          <Col span={24} style={{ marginTop: "7px" }}>
+            <Row key={exchange.id}>
+              <Col span={6}>
+                <Text>
+                  <strong>{exchange.rank}.</strong>
+                </Text>
+                <Avatar className="exchange-image" src={exchange.iconUrl} />
+                <Text>
+                  <strong>{exchange.name}</strong>
+                </Text>
+              </Col>
+              <Col span={6} style={{textAlign:'center'}}>${millify(exchange.volume)}</Col>
+              <Col span={6} style={{textAlign:'center'}}>{millify(exchange.numberOfMarkets)}</Col>
+              <Col span={6} style={{textAlign:'center'}}>{millify(exchange.marketShare)}%</Col>
+            </Row>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default Exchanges;
