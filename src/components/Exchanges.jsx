92f68f8c79cbeacc57dd748b2b40a009a41b210a
Allam Vachaspati <allamvachaspati05@gmail.com>
2021-12-11 15:21:43 +0530

    build fixes

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,13 +1,11 @@
 import React,{useEffect} from "react";
 import millify from "millify";
-import { Collapse, Row, Col, Typography, Avatar } from "antd";
-import HTMLReactParser from "html-react-parser";
+import {Row, Col, Typography, Avatar } from "antd";
 
 import { useGetExchangesQuery } from "../services/cryptoApi";
 import Loader from "./Loader";
 
 const { Text,Title } = Typography;
-const { Panel } = Collapse;
 
 const Exchanges = () => {
   const { data, isFetching } = useGetExchangesQuery();
